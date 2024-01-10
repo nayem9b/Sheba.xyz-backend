b671e32f0f1848e4fbf43ee30bc75b0bb8942cdc
Md Razwan Niam <razwanniam1@gmail.com>
2024-01-10 07:04:34 +0600

    payment controller

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,20 +1,51 @@
 /* eslint-disable no-undef */
 import dotenv from "dotenv";
 import path from "path";
+import { z } from "zod";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
+const envVarsZodSchema = z.object({
+  NODE_ENV: z.string(),
+  PORT: z
+    .string()
+    .default("3000")
+    .refine((val) => Number(val)),
+  DATABASE_URL: z.string(),
+  REDIS_URL: z.string(),
+  JWT_SECRET: z.string(),
+  STORE_ID: z.string(),
+  STORE_PASS: z.string(),
+  SSL_BASE_PAYMENT_URL: z.string(),
+  SSL_BASE_VALIDATION_URL: z.string(),
+  BCRYPT_SALT_ROUNDS: z.number(),
+  ACCESS_SECRET: z.string(),
+  REFRESH_SECRET: z.string(),
+  JWT_ACCESS_EXPIRES_IN: z.string(),
+  JWT_REFRESH_EXPIRES_IN: z.string(),
+  AWS_ACCESS_ID: z.string(),
+  AWS_ACCESS_KEY: z.string(),
+});
+
+const envVars = envVarsZodSchema.parse(process.env);
+
 export default {
-  env: process.env.NODE_ENV,
-  port: process.env.PORT,
-  database_url: process.env.DATABASE_URL,
-  bycrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  database_url: envVars.DATABASE_URL,
+  bycrypt_salt_rounds: envVars.BCRYPT_SALT_ROUNDS,
   jwt: {
-    access_secret: process.env.ACCESS_SECRET,
-    refresh_secret: process.env.REFRESH_SECRET,
-    access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
-    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+    access_secret: envVars.ACCESS_SECRET,
+    refresh_secret: envVars.REFRESH_SECRET,
+    access_expires_in: envVars.JWT_ACCESS_EXPIRES_IN,
+    refresh_expires_in: envVars.JWT_REFRESH_EXPIRES_IN,
   },
-  AWS_ACCESS_ID: process.env.AWS_ACCESS_ID,
-  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+  AWS_ACCESS_ID: envVars.AWS_ACCESS_ID,
+  AWS_ACCESS_KEY: envVars.AWS_ACCESS_KEY,
+  ssl: {
+    storeId: envVars.STORE_ID,
+    storePass: envVars.STORE_PASS,
+    sslPaymentUrl: envVars.SSL_BASE_PAYMENT_URL,
+    sslValidationUrl: envVars.SSL_BASE_VALIDATION_URL,
+  },
 };
---
+++ src/module/payment/payment.controller.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import sendResponse from "../../shared/sendResponse";
+import httpStatus from "http-status";
+import { PaymentService } from "./payment.service";
+
+const initPayment = async (req: Request, res: Response, next: NextFunction) => {
+  const result = await PaymentService.initPayment(req.body);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Payment init successfully",
+    data: result,
+  });
+};
